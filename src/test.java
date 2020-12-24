a1fb23dcaa5a4cab632abeacee2fd4be25b86aaa
AngusLiu <angus09855069@gmail.com>
2020-12-25 02:05:50 +0800

    新增Test類別

--- src/test.java
+++ src/test.java
@@ -3,4 +3,31 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("123");
 	}
+
+	class Test {
+		private int testNumber;
+		private String testString;
+
+		Test(int testNumber, String testString) {
+			this.testNumber = testNumber;
+			this.testString = testString;
+		}
+
+		public int getTestNumber() {
+			return testNumber;
+		}
+
+		public void setTestNumber(int testNumber) {
+			this.testNumber = testNumber;
+		}
+
+		public String getTestString() {
+			return testString;
+		}
+
+		public void setTestString(String testString) {
+			this.testString = testString;
+		}
+
+	}
 }
