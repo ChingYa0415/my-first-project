a052ea05d8ba102285b4fa28c35bdd3a988b9d3b
AngusLiu <angus09855069@gmail.com>
2020-12-25 02:23:38 +0800

    新增DifferentRow類別，測試不同行數問題

--- src/test.java
+++ src/test.java
@@ -3,41 +3,57 @@ public class test {
 
 	public static void main(String[] args) {
 		System.out.println("123");
-		Test test = new Test(1, "Test Number One");
+		
 	}
 
-	static class Test {
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	class DifferentRow {
 		private int testNumber;
-		private String testString;
 
-		Test(int testNumber, String testString) {
-			this.testNumber = testNumber;
-			this.testString = testString;
-		}
-
-		public int getTestNumber() {
-			return testNumber;
-		}
-
-		public void setTestNumber(int testNumber) {
-			this.testNumber = testNumber;
-		}
-
-		public String getTestString() {
-			return testString;
-		}
-
-		public void setTestString(String testString) {
-			this.testString = testString;
-		}
-
-	}
-
-	class Validation {
-
-		private int testNumber;
-
-		public Validation(int testNumber) {
+		public DifferentRow(int testNumber) {
 			super();
 			this.testNumber = testNumber;
 		}
