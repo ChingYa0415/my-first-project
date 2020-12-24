6c648894d628e97d5d871514c6f904dff8c72115
AngusLiu <angus09855069@gmail.com>
2020-12-25 02:28:38 +0800

    使用setter更改至2號測試

--- src/test.java
+++ src/test.java
@@ -4,6 +4,8 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("123");
 		Test test = new Test(1, "Test Number One");
+		test.setTestNumber(2);
+		test.setTestString("Test Number Two");
 	}
 
 	static class Test {
